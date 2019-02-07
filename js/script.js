38a8eef988821922efe001345ef690f49b1a5520
TanzinaTanny <tanzinatanny@gmail.com>
2019-02-07 17:49:34 -0500

    slideshow working, images need to scale inside, turn.js not working.

---
+++ js/script.js
@@ -0,0 +1,84 @@
+'use strict';
+// SLIDESHOW
+$(function() {
+
+    //settings for slider
+    var width = 720;
+    var animationSpeed = 1000;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+
+
+});
+
+// END OF SLIDESHOW
+
+// TURN.JS FLIPBOOK
+
+  $(window).ready(function(){
+      $('#book').turn({
+        display: 'double',
+        acceleration: true,
+        elevation: 50
+      });
+  });
+
+
+  $(window).bind('keydown', function(e){
+
+      if (e.keyCode==37)
+        $('#book').turn('previous');
+      else if (e.keyCode == 39)
+        $('#book').turn('next');
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // end
